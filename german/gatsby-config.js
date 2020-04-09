44d87ff69921e9aaf20d4dcf8ead048215acb973
Tom Barton <tom.r.barton@gmail.com>
2020-04-09 14:08:40 +0100

    Update README

--- german/gatsby-config.js
+++ german/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby WP Concept',
     description: 'Proof of concept for sourcing WordPress content via Gatsby.',
-    author: '@rawnet',
+    author: 'Tom Barton',
   },
   plugins: [{ resolve: `gatsby-theme-minimal`, options: { language: 'de' } }],
 }
